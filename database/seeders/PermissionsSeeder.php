473d38fd938ec18f4671406be7f4213473a4ab87
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2024-03-15 16:57:05 +0200

    wip
    add seeders

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[
+        \Spatie\Permission\PermissionRegistrar::class
+        ]->forgetCachedPermissions();
+
+        // create permissions
+
+    }
+}
---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['Student', 'AS', 'IS', 'SuperAdmin'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+    }
+}
