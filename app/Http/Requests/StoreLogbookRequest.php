e2313103f053eb7f8fbc797db8518f836656e2c3
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2024-03-18 19:58:37 +0200

    wip
    Storerequest

--- app/Http/Requests/StoreLogbookRequest.php
+++ app/Http/Requests/StoreLogbookRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreLogbookRequest extends FormRequest
@@ -11,18 +12,20 @@ class StoreLogbookRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'date' => ['required'],
+            'task' => ['required'],
+            'completed' => ['required'],
         ];
     }
 }
