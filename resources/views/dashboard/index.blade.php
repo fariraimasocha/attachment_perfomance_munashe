f90058b40d140c7c156d58d8c7bb9c74add2b7f2
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2024-03-21 15:39:31 +0200

    wip
    counting in db

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,29 +1,71 @@
-@include('layout.app')
+
 <x-layout>
+    <div class="px-40 mt-10 to-black1 flex justify-between">
+        @php
+            $totalLogs = \App\Models\Obook::count();
+        @endphp
 
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Chart Sample</title>
-        <script src="{{ $chart->cdn() }}"></script>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    </head>
-    <body class="h-screen bg-gray-100">
-
-    <div class="container px-4 mx-auto">
-
-        <div class="p-6 m-20 bg-white rounded shadow">
-            {!! $chart->container() !!}
+        <div class="bg-blue1 w-2/12 rounded py-3 hover:bg-black1 transition">
+            <p class="text-white text-center">
+                Total Logs
+            </p>
+            <h1 class="text-white text-center text-2xl font-semi">
+                {{ $totalLogs }}
+            </h1>
         </div>
 
+        @php
+            $totalUsers = \App\Models\User::count();
+        @endphp
+
+        <div class="bg-blue1 w-2/12 rounded py-3 hover:bg-black1 transition">
+            <p class="text-white text-center">
+                Total Users
+            </p>
+            <h1 class="text-white text-center text-2xl font-semi">
+                {{ $totalUsers }}
+            </h1>
+        </div>
+
+        @php
+            $totalReports = \App\Models\Report::count();
+        @endphp
+
+        <div class="bg-blue1 w-2/12 rounded py-3 hover:bg-black1 transition">
+            <p class="text-white text-center">
+                Total Reports
+            </p>
+            <h1 class="text-white text-center text-2xl font-semi">
+                {{ $totalReports }}
+            </h1>
+        </div>
+
+        @php
+            $totalPermissions= Spatie\Permission\Models\Permission::count();
+        @endphp
+
+        <div class="bg-blue1 w-2/12 rounded py-3 hover:bg-black1 transition">
+            <p class="text-white text-center">
+                Total Permissions
+            </p>
+            <h1 class="text-white text-center text-2xl font-semi">
+                {{ $totalPermissions }}
+            </h1>
+        </div>
+
+
+        @php
+            $totalRoles = Spatie\Permission\Models\Role::count();
+        @endphp
+
+        <div class="bg-blue1 w-2/12 rounded py-3 hover:bg-black1 transition">
+            <p class="text-white text-center">
+                Total Roles
+            </p>
+            <h1 class="text-white text-center text-2xl font-semi">
+                {{ $totalRoles }}
+            </h1>
+        </div>
     </div>
 
-    <script src="{{ $chart->cdn() }}"></script>
-
-    {{ $chart->script() }}
-    </body>
-    </html>
 </x-layout>
